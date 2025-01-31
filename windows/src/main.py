25df67be0fd498e497cf68b23275752d3fc60900
GuySky <desyatov2005@gmail.com>
2025-02-01 01:24:28 +0300

    fullscreen, caption and icon set

--- windows/src/main.py
+++ windows/src/main.py
@@ -2,3 +2,33 @@ from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
 import pygame
+
+pygame.init()
+
+icon = pygame.image.load('windows/assets/images/icon.png')
+
+pygame.display.set_icon(icon)
+
+pygame.display.set_caption('windows template')
+
+screen = pygame.display.set_mode((1280, 720), vsync=1)
+
+clock = pygame.time.Clock()
+running = True
+
+pygame.display.toggle_fullscreen()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+
+    screen.fill("purple")
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
