e0a1f631126144969ee37fd6fe8fe55585a5506e
GuySky <desyatov2005@gmail.com>
2025-02-03 16:28:14 +0300

    minor comments changes

--- windows/src/windows.py
+++ windows/src/windows.py
@@ -18,10 +18,7 @@ pygame.init()
 # 1920, 1080
 # 2048, 1080
 # 3840, 2160
-
-WINDOW_WIDTH = 320
-WINDOW_HEIGHT = 180
-
+SCREEN_WIDTH, SCREEN_HEIGHT = 320, 180
 # ----====XXXXXXXX====----
 
 
@@ -30,19 +27,17 @@ WINDOW_HEIGHT = 180
 # pygame.RESIZABLE  -   borders of the window can be dragged to chenge its size
 # pygame.SCALED     -   resolution depends on desktop size
 # pygame.FULLSCREEN -   always fullscreen
-
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE | pygame.SCALED)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE | pygame.SCALED)
 pygame.display.toggle_fullscreen()
-
 # ----====XXXXXXXX====----
 
 
 
-icon = pygame.image.load('windows/assets/images/icon.png').convert_alpha()
-bg_image = pygame.image.load('windows/assets/images/bg1.png').convert()
+icon = pygame.image.load('windows/assets/images/icon.png').convert_alpha()      # window icon
+bg_image = pygame.image.load('windows/assets/images/bg1.png').convert_alpha()   # background image
 
 pygame.display.set_icon(icon)
-pygame.display.set_caption('windows template')
+pygame.display.set_caption('windows template')      # window caption
 
 clock = pygame.time.Clock()
 running = True
@@ -52,7 +47,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
+            if event.key == pygame.K_F11:
                 pygame.display.toggle_fullscreen()
             if event.key == pygame.K_ESCAPE:
                 running = False
@@ -60,7 +55,6 @@ while running:
     screen.blit(bg_image)
 
     pygame.display.flip()
-
     clock.tick(60)
 
 pygame.quit()
